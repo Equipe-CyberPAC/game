ec27a6d2b0535261f24b94f74add1e1abd1c3d5e
CodyKoInABox <codykoinabox@gmail.com>
2023-09-21 22:50:41 -0300

    Fix player gravity

--- src/Assets/Scripts/Player/Gravity.cs
+++ src/Assets/Scripts/Player/Gravity.cs
@@ -8,6 +8,11 @@ public class Gravity : MonoBehaviour
     public CharacterController controller;
     Vector3 velocity;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     public float gravity = -9.81f;
 
     // Start is called before the first frame update
@@ -19,6 +24,12 @@ public class Gravity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -3f;
+        }
+
         velocity.y += gravity * Time.deltaTime;
         
         controller.Move(velocity * Time.deltaTime);
