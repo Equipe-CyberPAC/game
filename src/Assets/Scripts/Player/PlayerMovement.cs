b052ce4cc91d8bc904a90fda6f0231cea8a14641
CodyKoInABox <codykoinabox@gmail.com>
2023-09-29 19:15:02 -0300

    changes

--- src/Assets/Scripts/Player/PlayerMovement.cs
+++ src/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,7 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController controller;
     public float speed = 12f;
-
+    public float sprintSpeed = 20f;
 
     // Update is called once per frame
     void Update()
@@ -17,6 +18,11 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.LeftShift)){
+            controller.Move(sprintSpeed * Time.deltaTime * move);
+        }else{
+            controller.Move(speed * Time.deltaTime * move);
+        }
+        
     }
 }
